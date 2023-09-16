772c4800355f780efeaef62425289663bd6140d2
Matheus Lino <matheus@mrlino.com>
2023-09-16 16:21:02 -0300

    style: run black, isort and flake8 on key_generation.py

--- shortame/key_generation.py
+++ shortame/key_generation.py
@@ -5,11 +5,14 @@ from string import ascii_letters, digits
 from loguru import logger
 from loguru._logger import Logger
 
+from shortame.adapters.dynamodb_adapter import (AbstractDynamoDBUrlTable,
+                                                ShortURLNotFound, UrlTable)
 from shortame.adapters.redis_adapter import AbstractUrlQueue, ShortUrlQueue
-from shortame.adapters.dynamodb_adapter import AbstractDynamoDBUrlTable, UrlTable, ShortURLNotFound
+
 
 class AbstractShortUrlGenerator(ABC):
     """Abstract Short Url Generator"""
+
     def __init__(self, queue: AbstractUrlQueue, table: AbstractDynamoDBUrlTable):
         self.queue = queue
         self.table = table
@@ -29,14 +32,15 @@ class AbstractShortUrlGenerator(ABC):
 
 class ShortUrlGenerator(AbstractShortUrlGenerator):
     """Encapsulates the logic for creating and enqueing short urls"""
+
     def __init__(self, logger: Logger = logger):
-        super().__init__(queue = ShortUrlQueue(), table = UrlTable())
+        super().__init__(queue=ShortUrlQueue(), table=UrlTable())
         self.logger = logger
-    
+
     def generate_and_enque(self, size: int = 7) -> None:
         """
         Generate a new short url and enqueue it if it's not already present in the database.
-        
+
         :param size: the size of the short url to be generated
         """
         self.logger.info(f"Generating a new short url with size {size}")
@@ -51,18 +55,20 @@ class ShortUrlGenerator(AbstractShortUrlGenerator):
         :param short_url: the short url to be verified, e.g.: xyz1234
         :return exists: states if the short url is present or not
         """
-        self.logger.info(f"Verifying the existence of key {short_url} on table {self.table.table_name}")
+        self.logger.info(
+            f"Verifying the existence of key {short_url} on table {self.table.table_name}"
+        )
         try:
             self.table.get_url(short_url=short_url)
-        except ShortURLNotFound as s:
+        except ShortURLNotFound:
             self.logger.info(f"Short url '{short_url}' is good to go")
             return False
         except Exception as e:
-            pass
+            raise e
         else:
             self.logger.info(f"Short url '{short_url}' is already taken")
             return True
-    
+
     @staticmethod
     def generate(size: int = 7) -> str:
         """
@@ -72,5 +78,5 @@ class ShortUrlGenerator(AbstractShortUrlGenerator):
         :return short_url: the new url, e.g.: xyz1234
 
         """
-        short_url = ''.join((choice(ascii_letters + digits) for i in range(size)))
+        short_url = "".join((choice(ascii_letters + digits) for i in range(size)))
         return short_url
