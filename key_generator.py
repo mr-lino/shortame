a3e8bcbd5e91423d0a43fa4b830eb4957faa1f71
Matheus Lino <matheus@mrlino.com>
2023-09-16 16:44:44 -0300

    feat: add key_generator file to execute the key generation process

---
+++ key_generator.py
@@ -0,0 +1,20 @@
+from time import sleep
+from loguru import logger
+from shortame.key_generation import ShortUrlGenerator
+from shortame.config import settings
+
+
+if __name__ == '__main__':
+    logger.add(sink="key_generator.log")
+    logger.info("Executing the Key Generator service")
+    keygen = ShortUrlGenerator()
+    minimum_size = settings.short_url_minimum_queue_size
+    while True:
+        logger.info("Comparing the current queue size with the minimum size")
+        if keygen.queue.current_size() <= minimum_size:
+            logger.info(f"Queue size is less than {minimum_size}")
+            keygen.generate_and_enque()
+        else:
+            logger.info("Queue size is greather than minimum size")
+            logger.info("Sleeping for 5 seconds")
+            sleep(5)
--- shortame/config.py
+++ shortame/config.py
@@ -1,8 +1,14 @@
+from os import environ
+
 from dynaconf import Dynaconf
 
 settings = Dynaconf(
     root_path="../",
     settings_files=["settings.toml", ".secrets.toml"],
     environments=True,
-    load_dotenv=True
+    load_dotenv=True,
 )
+
+
+def get_redis_host_and_port():
+    return {"host": settings.redis_host, "port": settings.redis_port}
