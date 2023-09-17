1bc457fe4ad62f542e4b1f7cb68336e675e11898
Matheus Lino <matheus@mrlino.com>
2023-09-17 13:35:14 -0300

    feat: add table existence verification on create_dynamodb_locally

--- create_dynamodb_locally.py
+++ create_dynamodb_locally.py
@@ -1,8 +1,12 @@
 import boto3
+from botocore.exceptions import ClientError
+from loguru import logger
 
 from shortame.config import settings
 
-client = boto3.resource(
+logger.add(sink="create_dynamodb_locally.log")
+
+resource = boto3.resource(
     "dynamodb",
     endpoint_url=settings.dynamodb_endpoint,
     aws_access_key_id=settings.aws_access_key_id,
@@ -10,12 +14,21 @@ client = boto3.resource(
     region_name=settings.aws_region_name,
 )
 table_name = "url"
-table = client.create_table(
-    AttributeDefinitions=[
-        {"AttributeName": "short_url", "AttributeType": "S"},
-    ],
-    TableName=table_name,
-    KeySchema=[{"AttributeName": "short_url", "KeyType": "HASH"}],
-    BillingMode="PAY_PER_REQUEST",
-)
-table.wait_until_exists()
+logger.info(f"DynamoDB table name: '{table_name}'")
+try:
+    logger.info(f"Verifying if table '{table_name}' exists")
+    resource.Table(table_name).load()
+    logger.info(f"Table '{table_name}' was already created")
+except ClientError as e:
+    if e.response["Error"]["Code"] == "ResourceNotFoundException":
+        logger.info(f"Table '{table_name}' does not exists, creating it now")
+        table = resource.create_table(
+            AttributeDefinitions=[
+                {"AttributeName": "short_url", "AttributeType": "S"},
+            ],
+            TableName=table_name,
+            KeySchema=[{"AttributeName": "short_url", "KeyType": "HASH"}],
+            BillingMode="PAY_PER_REQUEST",
+        )
+        table.wait_until_exists()
+        logger.info(f"Table {table_name} created")
